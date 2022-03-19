c1f65f0d7ea8da6542b0a04b3063305922e28010
Upterry <mattleroy11@gmail.com>
2022-03-19 18:39:09 -0400

    Added additional email field to user registration, added a bit of formatting with Crispy Forms
    Signed-off-by: Upterry <mattleroy11@gmail.com>

--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Two things are happening with this register function. We either generate a brand new form if it is a GET(else) request
 # Or if the request is POST, we are taking the data the user entered and giving it to the form
+# UserCreationForm is no longer used in our imports because we modified it to add an email (in users/forms)
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!') # This is a flash message to indicate the account is created
             return redirect('register')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})   # Left is a variable, right is what is instanced on line 5
