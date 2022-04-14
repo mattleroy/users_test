ec880a39bb039c23b2839e4510cc7c4a85596e8b
Upterry <mattleroy11@gmail.com>
2022-04-14 15:40:31 -0400

    Finished simple register, login, logout, index pages for a user registration app.
    Signed-off-by: Upterry <mattleroy11@gmail.com>

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from .forms import UserRegistrationForm
 # Two things are happening with this register function. We either generate a brand new form if it is a GET(else) request
 # Or if the request is POST, we are taking the data the user entered and giving it to the form
 # UserCreationForm is no longer used in our imports because we modified it to add an email (in users/forms)
+# (Leaving it imported for informational purposes)
 
 def register(request):
     if request.method == "POST":
@@ -21,3 +22,4 @@ def register(request):
 
 def index(request):
     return render(request, 'users/index.html')
+
